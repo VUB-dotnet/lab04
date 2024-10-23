28eabd415658d9bffa75fe2a549d9eaf2a5f3f42
Krunoslav Husak <khusak@vub.hr>
2024-10-23 07:49:51 +0000

    Scaffoldan model i napravljene migracije

---
+++ Migrations/20241023073934_AddDescriptionToTodo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Todo",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Todo");
+        }
+    }
+}
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -3,5 +3,6 @@ namespace TodoerApp.Models;
 public class Todo {
     public int Id { get; set; }
     public string? Title { get; set; }
+    public string? Description { get; set; }
     public bool IsCompleted { get; set; }
 }
